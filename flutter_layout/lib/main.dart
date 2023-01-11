74464367d34bc88cc06641fbe30049635fe5612a
tyih <tyih@icloud.com>
2023-01-11 17:36:59 +0800

    子控件在父容器中的布局

--- flutter_layout/lib/main.dart
+++ flutter_layout/lib/main.dart
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +39,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Stack(
+          children: <Widget>[
+            Container(color: Colors.yellow, width: 300, height: 300),
+            const Positioned(
+                left: 18.0, top: 100.0, child: Text('Stack提供了层叠布局的容器')),
+            Positioned(
+                left: 18.0,
+                top: 100.0,
+                child: Container(
+                  color: Colors.green,
+                  width: 50,
+                  height: 50,
+                ))
+          ],
+        ));
+    // body: Column(
+    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //   mainAxisSize: MainAxisSize.min,
+    //   children: <Widget>[
+    //     Container(color: Colors.yellow, width: 60, height: 80),
+    //     Container(color: Colors.red, width: 100, height: 180),
+    //     Container(color: Colors.black, width: 60, height: 80),
+    //     Container(color: Colors.green, width: 60, height: 80)
+    //   ],
+    // ));
+    // body: Row(
+    //   children: <Widget>[
+    //     Expanded(flex:1, child: Container(color: Colors.yellow, width: 60, height: 80)),
+    //     Container(color: Colors.red, width: 100, height: 180),
+    //     Container(color: Colors.black, width: 60, height: 80),
+    //     Expanded(flex:1, child: Container(color: Colors.green, width: 60, height: 80))
+    //   ],
+    // ));
+    // body: const Center(
+    //   child: Text('单子Widget布局容器Center将其子Widget居中排列'),
+    // ));
+    // body: const Padding(
+    //   padding: EdgeInsets.all(20.0),
+    //   child: Text('如果只要将子Widget设定间距，可以使用单子容器控件Padding进行内容填充'),
+    // ));
+    // body: Container(
+    //   padding: const EdgeInsets.all(10), // 内边距
+    //   margin: const EdgeInsets.all(10.0), // 外边距
+    //   width: 180.0,
+    //   height: 240.0,
+    //   alignment: Alignment.center,
+    //   decoration: BoxDecoration( // Container样式
+    //       color: Colors.red,
+    //       borderRadius: BorderRadius.circular(10.0)),
+    //   child: const Text('Container（容器）在UI框架中是一个很常见的概念，Flutter也不例外'),
+    // ));
   }
 }
